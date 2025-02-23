a5fa0a6c91cd6bb10e1bebd3b0531d6fcc022847
Manuel Ricardo Vargas Cruz <manuelricardovargascruz@gmail.com>
2025-02-23 13:19:52 -0500

    separación cod

---
+++ scripts/RegressionP3.R
@@ -0,0 +1,65 @@
+
+# 01_Cargar Librerias
+# library(tidyverse)
+# library(readr)
+# library(dplyr)
+# library(ggplot2)
+# library(rio)
+# library(skimr)
+# library(visdat)
+# library(stargazer)
+##########################################################
+# Title: Data Cleaning.
+# Description: This script webscrapes the data from the website
+# https://ignaciomsarmiento.github.io/GEIH2018_sample/ the
+# objective is to retrive 10 chunks of data from the website
+# corresponding to a sample of GEIH 2018. Each one would be
+# stored in a individual csv file.
+#
+# Date: 09/02/2025
+##########################################################
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  = = = = = = = = 
+# 0. Workspace configuration ====================================================================
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  = = = = = = = = 
+
+# Clear workspace
+
+rm(list = ls())
+
+# Set up paths
+
+dir <- list()
+dir$root <- getwd()
+dir$stores <- file.path(dir$root, "stores", "raw")
+dir$views <- file.path(dir$root, "views")
+dir$scripts <- file.path(dir$root, "scripts")
+setwd(dir$root)
+
+# Load required libraries
+
+source(file.path(dir$scripts, "00_load_requierments.R"))
+
+
+# 02_load clean data GEIH DB
+
+data_limpia <- read.csv('data_limpiaGEIH.csv')
+
+
+## 3
+
+# CONVERT TO HOUR WAGE AND LOG
+db_geih_7 <- db_geih_6 %>% mutate(logwage=log(ingtot/totalHoursWorked), age2=age^2)
+
+# REGRESSION
+reg1 <- lm(logwage~age+age2, data=db_geih_7)
+
+summary(reg1)
+
+ggplot(db_geih_7, aes(x = age, y = logwage)) +
+  geom_point(alpha = 0.5) +  
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "red", se = TRUE) +
+  labs(title = "Regresión",
+       x = "Age",
+       y = "log(W)") +
+  theme_minimal()
