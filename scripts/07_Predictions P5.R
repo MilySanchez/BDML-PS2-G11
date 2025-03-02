1f37cfb2367b8791c1e42bfbccd644a4e7eec336
Juanita Rojas <jrojascuellar@outlook.com>
2025-03-02 10:42:35 -0500

    Avance punto 5

--- scripts/07_Predictions P5.R
+++ scripts/07_Predictions P5.R
@@ -27,8 +27,6 @@ setwd(dir$root)
 
 source(file.path(dir$scripts, "00_load_requierments.R"))
 
-
-
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
 # 1. Load data =================================================================
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
@@ -37,12 +35,10 @@ source(file.path(dir$scripts, "00_load_requierments.R"))
 
 data_clean <- read.csv(file.path(dir$processed,'data_cleanGEIH.csv'))
 
-data_clean <- data_clean  %>% mutate(sex=as.factor(sex),
-                                     estrato1=as.factor(estrato1),
+data_clean <- data_clean  %>% mutate(estrato1=as.factor(estrato1),
                                      p6050=as.factor(p6050),
                                      p6090=as.factor(p6090),
                                      p6210=as.factor(p6210),
-                                     p6210s1=as.factor(p6210s1),
                                      p6240=as.factor(p6240),
                                      p7495=as.factor(p7495),
                                      p7505=as.factor(p7505),
@@ -50,8 +46,9 @@ data_clean <- data_clean  %>% mutate(sex=as.factor(sex),
                                      formal=as.factor(formal),
                                      oficio=as.factor(oficio),
                                      cotPension=as.factor(cotPension),
-                                     ingtot_H=ingtot/totalHoursWorked,
                                      p6870 = as.factor(p6870))
+
+data_clean <- data_clean %>% mutate(p6240 = relevel(p6240, ref = 2))
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
 # 2. Split the sample =========================================================
@@ -81,63 +78,102 @@ testing <- data_clean %>%
 # 3. Compare the predictive performance of specifications =====================
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 
+# Define several model specifications
+
 model_1 <- lm(logwage ~ age + age2, data = training)
 
-model_2 <- lm(logwage ~ female*age + female*age2 + estrato1 + p6240 + 
+model_2 <- lm(logwage ~ female, data = training)
+
+model_3 <- lm(logwage ~ female + age + age2 + female:age + 
+                female:age2 + estrato1 + p6240 + 
                 p6426 + p6870 + cotPension + 
                 p7040 + p7495 + p7505, data = training)
 
-model_3 <- lm(logwage ~ female*age + female:age2 +female:poly(age,3,raw=T) + estrato1 + p6240 + 
-                p6426 + p6870 + cotPension + 
+model_4 <- lm(logwage ~ female + age + age2 + female:age +
+                female:age2 + poly(age, 3, raw = T) + estrato1 + 
+                p6240 + p6426 + p6870 + cotPension + 
                 p7040 + p7495 + p7505, data = training)
 
-model_4 <- lm(logwage ~ female:cotPension + female:p6870 + female + cotPension + 
-                p6870  + estrato1 + p6240 + p6426 + 
-                 + cotPension + p7040 + p7495 + p7505 + age + age2, data = training)
+model_5 <- lm(logwage ~ female:cotPension + female:p6870 + female + 
+                cotPension + p6870  + estrato1 + p6240 + p6426 + 
+                p7040 + p7495 + p7505 + age + age2, data = training)
 
-model_5 <- lm(logwage ~ age + age2 + poly(age,3,raw=T) + poly(age,4,raw=T) + 
-                poly(age,5,raw=T)  + poly(age,6,raw=T) + poly(age,7,raw=T) + poly(age,8,raw=T)
-              + poly(age,9,raw=T) + poly(age,10,raw=T) , data = training)
+model_6 <- lm(logwage ~ age + age2 + poly(age,3,raw=T) + poly(age,4,raw=T) + 
+                poly(age,5,raw=T)  + poly(age,6,raw=T) + poly(age,7,raw=T) + 
+                poly(age,8,raw=T) + poly(age,9,raw=T) + poly(age,10,raw=T), 
+              data = training)
 
-model_6 <- lm(logwage ~ age + age2 + p6870:age + p6870:totalHoursWorked + totalHoursWorked +
-                poly(totalHoursWorked,2,raw=T), data = training)
+model_7 <- lm(logwage ~ age + age2 + p6870:age + p6870:totalHoursWorked + 
+                totalHoursWorked + poly(totalHoursWorked, 2, raw=T), 
+              data = training)
 
 training <- training %>% mutate(logP6426 = ifelse(p6426 == 0, 0, log(p6426)))
 testing <- testing %>% mutate(logP6426 = ifelse(p6426 == 0, 0, log(p6426)))
                 
-model_7 <- lm(logwage ~ age + age2 + p6426 + poly(p6426,2,raw=T) + logP6426+ 
+model_8 <- lm(logwage ~ age + age2 + p6426 + poly(p6426,2,raw=T) + logP6426+ 
                 p6870 + estrato1 + p6240 + p6426 + abs(age-53)+
                 + cotPension + p7040 + p7495 + p7505, data = training)
 
-RMSE_values <- numeric(7)  
+# Create
 
+RMSE_values <- numeric(8)
 
-for (i in 1:7) {
+# Calculate the RMSE for each model
+ 
+for (i in 1:8) {
   model <- get(paste0('model_', i))
-  pred <- predict(model, testing)
+  pred <- predict(model, newdata = testing)
   RMSE_values[i] <- caret::RMSE(pred, testing$logwage)
 }
 
-#modelos 2 y 4
-ctrl <- trainControl(
-  method="LOOCV"
-)
+RMSE_values
 
-model_2b <- train(
-  logwage ~ female*age + female*age2 + estrato1 + p6240 + 
-    p6426 + p6870 + cotPension + 
-    p7040 + p7495 + p7505,
-  data=data_clean,
-  method="lm",
-  trControl=ctrl
+# Compute the prediction errors in the test sample
+
+best_pred <- predict(model_5, newdata = testing)
+best_residuals <- testing$logwage - best_pred
+
+# Examine its distribution
+
+hist(best_residuals, breaks = 30, main = 'Distribution of Prediction Errors')
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
+# 4. Calculate predictive error using LOOCV ===================================
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+# Define LOOCV as the method for cross validation approach
+ctrl <- trainControl(
+  method = "LOOCV")
+
+# Enable progress printing
+ctrl$verboseIter <- T
+
+# Train models
+model_3b <- train(
+  logwage ~ female + age + age2 + female:age + female:age2 + 
+    estrato1 + p6240 + p6426 + p6870 + cotPension + p7040 + 
+    p7495 + p7505, 
+  data = data_clean,
+  method = "lm",
+  trControl = ctrl
 )
 
 model_4b <- train(
+  logwage ~ female + age + age2 + female:age + female:age2 + 
+    poly(age, 3, raw = TRUE) + estrato1 + p6240 + p6426 + 
+    p6870 + cotPension + p7040 + p7495 + p7505, 
+  data = data_clean,
+  method = "lm",
+  trControl = ctrl,
+  metric = 'RMSE'
+)
+
+model_5b <- train(
   logwage ~ female:cotPension + female:p6870 + female + cotPension + 
-    p6870  + estrato1 + p6240 + p6426 + 
-    + cotPension + p7040 + p7495 + p7505 + age + age2,
-  data=data_clean,
-  method="lm",
-  trControl=ctrl
+    p6870  + estrato1 + p6240 + p6426 + p7040 + p7495 + p7505 + age + age2,
+  data = data_clean,
+  method = "lm",
+  trControl = ctrl,
+  metric = 'RMSE'
 ) 
 
