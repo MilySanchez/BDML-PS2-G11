a10126ef4cc277bc9b235692bcda1c3cef6eb29d
MilySanchez <milygeraldinsanchezjulio@hotmail.com>
2025-04-13 16:21:43 -0500

    Add files via upload

--- scripts/EN_lambda_0_01_alpha_0_2 - V2.R
+++ scripts/EN_lambda_0_01_alpha_0_2 - V2.R
@@ -15,8 +15,8 @@ rm(list = ls())
 
 dir <- list()
 dir$root <- getwd()
-dir$stores <- file.path(dir$root, "stores", "raw")
-dir$processed <- file.path(dir$root, "stores", "processed")
+dir$raw <- file.path(dir$root, "store", "raw")
+dir$processed <- file.path(dir$root, "store", "processed")
 dir$views <- file.path(dir$root, "views")
 dir$scripts <- file.path(dir$root, "scripts")
 dir$results <- file.path(dir$root, "results")
@@ -26,96 +26,17 @@ setwd(dir$root)
 
 source(file.path(dir$scripts, "00_load_requierments.R"))
 
-train_hogares<-read.csv(file.path(dir$stores, "train_hogares.csv"))
-train_personas<-read.csv(file.path(dir$stores, "train_personas.csv"))
+train <- read.csv(file.path(dir$processed, "train.csv")) |> select(c(Clase, Dominio, Cuartos, CuartosDormir, TenenciaVivienda, 
+                                                                     Npersonas, NpersonasUG, Lindigencia, Lpobreza, Fex_c, Depto, Fex_dpto, 
+                                                                     Pobre, arriendo, H_Head_Mujer, H_Head_EducLevel, 
+                                                                     H_Head_Ocupado, H_Head_CotPen, H_Head_Prima, H_Head_RegSalud,H_Head_Edad, H_Head_HorasT,
+                                                                     nOcupado, maxEducLevel, nCotPen, nPrima, nSubsidiado))
 
-train_personas <- train_personas |> 
-  select(id, Orden, Clase, Dominio, P6020, P6040, P6050, P6090, P6100, P6210, 
-         P6210s1, P6240, Oficio, P6426, P6430, P6510, P6545, P6580, P6585s1, 
-         P6585s2, P6585s3, P6585s4, P6590, P6600, P6610, P6620, P6630s1, 
-         P6630s2, P6630s3, P6630s4, P6630s6, P6800, P6870, P6920, P7040, 
-         P7045, P7050, P7090, P7110, P7120, P7150, P7160, P7310, P7350, 
-         P7422, P7472, P7495, P7500s2, P7500s3, P7505, P7510s1, P7510s2, 
-         P7510s3, P7510s5, P7510s6, P7510s7, Pet, Oc, Des, Ina, Fex_c, 
-         Depto, Fex_dpto)
-
-train_hogares <- train_hogares |> 
-  select(id, Clase, Dominio, P5000, P5010, P5090, P5100, P5130, P5140, Nper, 
-         Npersug, Li, Lp, Fex_c, Depto, Fex_dpto, Pobre) |> 
-  mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),Dominio=factor(Dominio)) |>
-  mutate(arriendo=ifelse(!is.na(P5140),P5140,ifelse(!is.na(P5100),P5100,P5130)))
-
-train_hogares <- upSample(x = train_hogares |> select(-Pobre), y = train_hogares$Pobre, yname="Pobre")
-
-train_personas<- train_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), 
-  H_Head = ifelse(P6050== 1, 1, 0),
-  EducLevel = ifelse(P6210==9,0,P6210),
-  ocupado = ifelse(is.na(Oc),0,1),
-  CotPen = ifelse(is.na(P6920),0,ifelse(P6920==1,1,0)),
-  Prima = ifelse(is.na(P6630s1),0,ifelse(P6630s1==1,1,0)),
-  Edad = P6040,
-  RegSalud = ifelse(is.na(P6100)|P6100==4,0,P6100),
-  HorasT = ifelse(is.na(P6800),0,P6800)
-) |> select(id, Orden, mujer, H_Head, EducLevel, ocupado, CotPen, Prima, RegSalud, Edad, HorasT)
-
-
-train_personas_nivel_hogar <- train_personas |>
-  group_by(id) |> summarize (
-    nocupado=sum(ocupado, na.rm=T),
-    maxEducLevel=max(EducLevel,na.rm=TRUE),
-    ncotpen=sum(CotPen, na.rm=TRUE),
-    nprima=sum(Prima, na.rm=TRUE),
-    nSubsidiado = sum(RegSalud==3, na.rm=T),
-  )
-
-train_personas_hogar <- train_personas |> filter(H_Head==1) |>
-  select(id, mujer, EducLevel, ocupado, CotPen, Prima, RegSalud, Edad, HorasT) |>
-  rename(H_Head_mujer=mujer, H_Head_Edu=EducLevel, H_Head_ocupado = ocupado,
-         H_Head_cotpen=CotPen, H_Head_prima=Prima, H_Head_RedSalud=RegSalud,
-         H_Head_Edad=Edad, H_Head_HorasT=HorasT) |> 
-  left_join(train_personas_nivel_hogar)
-
-#TEST
-
-test_hogares<-read.csv(file.path(dir$stores, "test_hogares.csv"))
-test_personas<-read.csv(file.path(dir$stores, "test_personas.csv"))
-
-test_hogares <- test_hogares |>
-  mutate(arriendo=ifelse(!is.na(P5140),P5140,ifelse(!is.na(P5100),P5100,P5130)))
-
-test_personas<- test_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), 
-  H_Head = ifelse(P6050== 1, 1, 0),
-  EducLevel = ifelse(P6210==9,0,P6210),
-  ocupado = ifelse(is.na(Oc),0,1),
-  CotPen = ifelse(is.na(P6920),0,ifelse(P6920==1,1,0)),
-  Prima = ifelse(is.na(P6630s1),0,ifelse(P6630s1==1,1,0)),
-  Edad = P6040,
-  RegSalud = ifelse(is.na(P6100)|P6100==4,0,P6100),
-  HorasT = ifelse(is.na(P6800),0,P6800)
-) |> select(id, Orden, mujer, H_Head, EducLevel, ocupado, CotPen, Prima, RegSalud, Edad, HorasT)
-
-test_personas_nivel_hogar <- test_personas |>
-  group_by(id) |> summarize (
-    nocupado=sum(ocupado, na.rm=T),
-    maxEducLevel=max(EducLevel,na.rm=TRUE),
-    ncotpen=sum(CotPen, na.rm=TRUE),
-    nprima=sum(Prima, na.rm=TRUE),
-    nSubsidiado = sum(RegSalud==3, na.rm=T),
-  )
-test_personas_hogar <- test_personas |> filter(H_Head==1) |>
-  select(id, mujer, EducLevel, ocupado, CotPen, Prima, RegSalud, Edad, HorasT) |>
-  rename(H_Head_mujer=mujer, H_Head_Edu=EducLevel, H_Head_ocupado = ocupado,
-         H_Head_cotpen=CotPen, H_Head_prima=Prima, H_Head_RedSalud=RegSalud,
-         H_Head_Edad=Edad, H_Head_HorasT=HorasT) |> 
-  left_join(test_personas_nivel_hogar)
-
-#Data para el modelo
-
-train <- train_hogares |> left_join(train_personas_hogar) |> select(-c(id, P5100, P5130, P5140))
-test <- test_hogares |> left_join(test_personas_hogar) |> 
-  mutate(Dominio=factor(Dominio)) |> select(-c(P5100, P5130, P5140))
+test <- read.csv(file.path(dir$processed, "test.csv")) |> select(c(Clase, Dominio, Cuartos, CuartosDormir, TenenciaVivienda, 
+                                                                   Npersonas, NpersonasUG, Lindigencia, Lpobreza, Fex_c, Depto, Fex_dpto, 
+                                                                   arriendo, H_Head_Mujer, H_Head_EducLevel, 
+                                                                   H_Head_Ocupado, H_Head_CotPen, H_Head_Prima, H_Head_RegSalud,H_Head_Edad, H_Head_HorasT,
+                                                                   nOcupado, maxEducLevel, nCotPen, nPrima, nSubsidiado))
 
 
 #MODELO
--- scripts/EN_lambda_0_01_alpha_0_2.R
+++ scripts/EN_lambda_0_01_alpha_0_2.R
@@ -2,14 +2,15 @@
 
 #Puntaje 0.3733
 
+
 rm(list = ls())
 
 # Set up paths
 
 dir <- list()
 dir$root <- getwd()
-dir$stores <- file.path(dir$root, "stores", "raw")
-dir$processed <- file.path(dir$root, "stores", "processed")
+dir$raw <- file.path(dir$root, "store", "raw")
+dir$processed <- file.path(dir$root, "store", "processed")
 dir$views <- file.path(dir$root, "views")
 dir$scripts <- file.path(dir$root, "scripts")
 dir$results <- file.path(dir$root, "results")
@@ -19,88 +20,15 @@ setwd(dir$root)
 
 source(file.path(dir$scripts, "00_load_requierments.R"))
 
-train_hogares<-read.csv(file.path(dir$stores, "train_hogares.csv"))
-train_personas<-read.csv(file.path(dir$stores, "train_personas.csv"))
+train <- read.csv(file.path(dir$processed, "train.csv")) |> select(c(Clase, Dominio, Cuartos, CuartosDormir, TenenciaVivienda, CuotaAmortizacion, ArriendoEst, ArriendoEfec,
+                                                                     Npersonas, NpersonasUG, Lindigencia, Lpobreza, Fex_c, Depto, Fex_dpto, 
+                                                                     Pobre, H_Head_Mujer, H_Head_EducLevel, 
+                                                                     H_Head_Ocupado, nOcupado, maxEducLevel ))
 
-train_personas <- train_personas |> 
-  select(id, Orden, Clase, Dominio, P6020, P6040, P6050, P6090, P6100, P6210, 
-         P6210s1, P6240, Oficio, P6426, P6430, P6510, P6545, P6580, P6585s1, 
-         P6585s2, P6585s3, P6585s4, P6590, P6600, P6610, P6620, P6630s1, 
-         P6630s2, P6630s3, P6630s4, P6630s6, P6800, P6870, P6920, P7040, 
-         P7045, P7050, P7090, P7110, P7120, P7150, P7160, P7310, P7350, 
-         P7422, P7472, P7495, P7500s2, P7500s3, P7505, P7510s1, P7510s2, 
-         P7510s3, P7510s5, P7510s6, P7510s7, Pet, Oc, Des, Ina, Fex_c, 
-         Depto, Fex_dpto)
-
-train_hogares <- train_hogares |> 
-  select(id, Clase, Dominio, P5000, P5010, P5090, P5100, P5130, P5140, Nper, 
-         Npersug, Li, Lp, Fex_c, Depto, Fex_dpto, Pobre) |> 
-  mutate(Pobre=factor(Pobre,levels=c(0,1),labels=c("No","Yes")),Dominio=factor(Dominio))
-
-#train_hogares2 <- train_hogares |> rbind(train_hogares |> filter(Pobre==1)) |> 
- # rbind(train_hogares |> filter(Pobre==1)) 
-
-train_hogares <- upSample(x = train_hogares |> select(-Pobre), y = train_hogares$Pobre, yname="Pobre")
-
-id_hogares_pobres = train_hogares |> filter(Pobre==1) |> select(id)
-
-personas_pobres = train_personas |> filter(id %in% id_hogares_pobres$id)
-
-train_personas<- train_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), 
-  H_Head = ifelse(P6050== 1, 1, 0),
-  EducLevel = ifelse(P6210==9,0,P6210),
-  ocupado = ifelse(is.na(Oc),0,1),
-  CotPen = ifelse(is.na(P6920),0,ifelse(P6920==1,1,0)),
-  Prima = ifelse(is.na(P6630s1),0,ifelse(P6630s1==1,1,0))
-) |> select(id, Orden, mujer, H_Head, EducLevel, ocupado)
-
-
-
-train_personas_nivel_hogar <- train_personas |>
-  group_by(id) |> summarize (
-    nocupado=sum(ocupado, na.rm=T),
-    maxEducLevel=max(EducLevel,na.rm=TRUE)
-  )
-
-train_personas_hogar <- train_personas |> filter(H_Head==1) |>
-  select(id, mujer, EducLevel, ocupado) |>
-  rename(H_Head_mujer=mujer, H_Head_Edu=EducLevel, H_Head_ocupado = ocupado) |> 
-  left_join(train_personas_nivel_hogar)
-
-#TEST
-
-test_hogares<-read.csv(file.path(dir$stores, "test_hogares.csv"))
-test_personas<-read.csv(file.path(dir$stores, "test_personas.csv"))
-
-test_personas<- test_personas %>% mutate(
-  mujer = ifelse(P6020==2,1,0), 
-  H_Head = ifelse(P6050== 1, 1, 0),
-  EducLevel = ifelse(P6210==6,1,0),
-  ocupado = ifelse(is.na(Oc),0,1),
-  CotPen = ifelse(is.na(P6920),0,ifelse(P6920==1,1,0)),
-  Prima = ifelse(is.na(P6630s1),0,ifelse(P6630s1==1,1,0))
-) |> select(id, Orden, mujer, H_Head, EducLevel, ocupado)
-
-test_personas_nivel_hogar <- test_personas |>
-  group_by(id) |> summarize (
-    nocupado=sum(ocupado, na.rm=T),
-    maxEducLevel=max(EducLevel,na.rm=TRUE)
-  )
-
-test_personas_hogar <- test_personas |> filter(H_Head==1) |>
-  select(id, mujer, EducLevel, ocupado) |>
-  rename(H_Head_mujer=mujer, H_Head_Edu=EducLevel, H_Head_ocupado = ocupado) |> 
-  left_join(test_personas_nivel_hogar)
-
-#Data para el modelo
-
-train <- train_hogares |> left_join(train_personas_hogar) |> select(-c(id)) |> 
-  mutate(P5100=ifelse(is.na(P5100),0,P5100),P5130=ifelse(is.na(P5130),0,P5130),P5140=ifelse(is.na(P5140),0,P5140))
-
-test <- test_hogares |> left_join(test_personas_hogar) |> 
-  mutate(P5100=ifelse(is.na(P5100),0,P5100),P5130=ifelse(is.na(P5130),0,P5130),P5140=ifelse(is.na(P5140),0,P5140)) |> 
-  mutate(Dominio=factor(Dominio))
+test <- read.csv(file.path(dir$processed, "test.csv")) |> select(c(Clase, Dominio, Cuartos, CuartosDormir, TenenciaVivienda, CuotaAmortizacion, ArriendoEst, ArriendoEfec,
+                                                                   Npersonas, NpersonasUG, Lindigencia, Lpobreza, Fex_c, Depto, Fex_dpto, 
+                                                                   H_Head_Mujer, H_Head_EducLevel, 
+                                                                   H_Head_Ocupado,nOcupado, maxEducLevel))
 
 
 #MODELO
